e8dacf386da0888f35a31050cc7f145e2f3af89f
Jirka <jirka.borovec@seznam.cz>
2021-11-26 00:36:16 +0100

    replace

--- detoxify/detoxify.py
+++ detoxify/detoxify.py
@@ -24,8 +24,7 @@ def get_model_and_tokenizer(
         local_files_only=pretrained_model_path is not None,
     )
     tokenizer = getattr(transformers, tokenizer_name).from_pretrained(
-        pretrained_model_name_or_path=pretrained_model_path,
-        model_type=model_type,
+        pretrained_model_path or model_type,
         local_files_only=pretrained_model_path is not None,
     )
 
